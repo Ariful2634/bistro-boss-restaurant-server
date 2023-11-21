bf68c2a0a790a10def7b5034109328191eb1c906
Ariful2634 <ariful2634@gmail.com>
2023-11-21 23:40:50 +0600

    post payment intent

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app  = express();
 const port = process.env.PORT || 5000;
 
@@ -217,19 +218,20 @@ async function run() {
       res.send(result)
     })
 
-  //   app.delete('/carts/:id', async(req,res)=>{
-  //     const id  = req.params.id;
-  //     const query ={_id: new ObjectId(id)}
-  //     const result = await cartsCollection.deleteOne(query);
-  //     res.send(result)
-  // })
 
-  // app.delete('/carts/:id', async(req,res)=>{
-  //   const id = req.params.id;
-  //   const query = {_id: new ObjectId(id)}
-  //   const result = await cartsCollection.deleteOne(query)
-  //   res.send(result)
-  // })
+
+    // payment intent
+
+    app.post('/create-payment-intent', async(req,res)=>{
+      const {price}=req.body;
+      const amount = parseInt(price*100)
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_mathod_types: ['card']
+      })
+    })
 
    
 
